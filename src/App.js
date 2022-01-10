4e1f7a82b5736f6811459534daa15ac13f1de8f7
mnskgrayce <mineskgrayce@gmail.com>
2022-01-10 13:49:13 +0700

    add console.log(token) to App.js

--- src/App.js
+++ src/App.js
@@ -1,60 +1,72 @@
-import Calendar from './pages/Calendar';
-import NavBar from './components/NavBar';
-import Profile from './pages/Profile';
-import Help from './pages/Help';
-import Home from './pages/Home';
-import MyPage from './pages/MyPage';
-import Register from './pages/Register';
-import Rental from './pages/Rental';
-import ViewDetail from './pages/ViewDetail';
-import BookMeeting from './pages/BookMeeting';
-import AdminCalendar from './pages/AdminCalendar';
-import AddHouse from './pages/AddHouse';
-import ViewRentalHouses from './pages/ViewRentalHouses';
-import ViewAllUsers from './pages/ViewAllUsers';
+import Calendar from "./pages/Calendar";
+import NavBar from "./components/NavBar";
+import Profile from "./pages/Profile";
+import Help from "./pages/Help";
+import Home from "./pages/Home";
+import MyPage from "./pages/MyPage";
+import Register from "./pages/Register";
+import Rental from "./pages/Rental";
+import ViewDetail from "./pages/ViewDetail";
+import BookMeeting from "./pages/BookMeeting";
+import AdminCalendar from "./pages/AdminCalendar";
+import AddHouse from "./pages/AddHouse";
+import ViewRentalHouses from "./pages/ViewRentalHouses";
+import ViewAllUsers from "./pages/ViewAllUsers";
 import React, { useEffect } from "react";
-import { Routes, Route } from 'react-router-dom';
-import Loader from './components/Loader';
-import Footer from './components/Footer'
-import { useAuth0 } from "@auth0/auth0-react"
-import axios from 'axios';
-import { useEnv } from './context/env.context';
+import { Routes, Route } from "react-router-dom";
+import Loader from "./components/Loader";
+import Footer from "./components/Footer";
+import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
+import { useEnv } from "./context/env.context";
 
 function App() {
-  const { isLoading, user,getAccessTokenSilently} = useAuth0()
-  const { apiServerUrl } = useEnv()
+  const { isLoading, user, getAccessTokenSilently } = useAuth0();
+  const { apiServerUrl } = useEnv();
 
-  useEffect(()=>{
-    const getUser = async () =>{
-      const token = await getAccessTokenSilently()
+  useEffect(() => {
+    const getUser = async () => {
+      const token = await getAccessTokenSilently();
+      console.log(token);
 
       // if userid is bigger than 21, they use oauth2
-      const currentUserId = user.sub.length < 21 ? user.sub.substring(user.sub.lastIndexOf("|")+1,user.sub.length) : Math.trunc(user.sub.substring(user.sub.lastIndexOf("|")+1,user.sub.length)/10000)
-      
+      const currentUserId =
+        user.sub.length < 21
+          ? user.sub.substring(user.sub.lastIndexOf("|") + 1, user.sub.length)
+          : Math.trunc(
+              user.sub.substring(
+                user.sub.lastIndexOf("|") + 1,
+                user.sub.length
+              ) / 10000
+            );
+
       // if user does not exist in the database
-      if (user.sub.length > 21){
+      if (user.sub.length > 21) {
         let data = {
-          "auth0Id":currentUserId,
-          "fullName": user.name,
-          "email": user.email
-        }
+          auth0Id: currentUserId,
+          fullName: user.name,
+          email: user.email,
+        };
         // Request made and server responded
-        await axios.post(`${apiServerUrl}/api/v1/users`,data,{
-          headers: {
-            authorization: `Bearer ${token}`
-          }
-        })
-        .then(()=>{})
-        .catch((err)=>{console.log(err)})
+        await axios
+          .post(`${apiServerUrl}/api/v1/users`, data, {
+            headers: {
+              authorization: `Bearer ${token}`,
+            },
+          })
+          .then(() => {})
+          .catch((err) => {
+            console.log(err);
+          });
       }
+    };
+    if (user !== undefined) {
+      getUser();
     }
-    if (user !== undefined){
-      getUser()
-    }
-  },[user,apiServerUrl,getAccessTokenSilently])
+  }, [user, apiServerUrl, getAccessTokenSilently]);
 
   if (isLoading) {
-    return <Loader />
+    return <Loader />;
   }
 
   return (
@@ -78,7 +90,11 @@ function App() {
         {/* admin routes */}
         <Route path="/auth/admin/calendar" exact element={<AdminCalendar />} />
         <Route path="/auth/admin/addHouse" exact element={<AddHouse />} />
-        <Route path="/auth/admin/viewRentalHouses" exact element={<ViewRentalHouses />} />
+        <Route
+          path="/auth/admin/viewRentalHouses"
+          exact
+          element={<ViewRentalHouses />}
+        />
         <Route path="/auth/admin/viewUsers" exact element={<ViewAllUsers />} />
 
         <Route path="*" component={() => "404 NOT FOUND"} />

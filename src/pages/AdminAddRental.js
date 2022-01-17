d4a09698344c7e7b2e65e80adda3066a3ed7859a
Phuong Nguyen <45571475+phu0n9@users.noreply.github.com>
2022-01-17 22:37:06 +0700

    Update AdminAddRental.js

--- src/pages/AdminAddRental.js
+++ src/pages/AdminAddRental.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { useEnv } from '../context/env.context'
 import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react"
@@ -29,36 +28,4 @@ const AdminAddRental = () => {
 
 export default withAuthenticationRequired(AdminAddRental, {
     onRedirecting: () => <Loader />,
-=======
-import React from 'react';
-import { useEnv } from '../context/env.context'
-import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react"
-import { Navigate } from 'react-router-dom';
-import Loader from '../components/Loader';
-import AddRentalForm from '../components/AddRentalForm';
-
-const AdminAddRental = () => {
-    const { user, getAccessTokenSilently } = useAuth0()
-    const { audience, apiServerUrl } = useEnv()
-    const role = `${audience}/roles`
-
-    // if logged in user is not admin
-    if (user[role].length === 0) {
-        return (
-            <>
-                <Navigate replace to="/unauthorized" />
-            </>
-        )
-    }
-
-    return (
-        <div style={{ marginTop: "100px" }}>
-            <AddRentalForm />
-        </div>
-    );
-};
-
-export default withAuthenticationRequired(AdminAddRental, {
-    onRedirecting: () => <Loader />,
->>>>>>> dc52a23 (add)
 })
